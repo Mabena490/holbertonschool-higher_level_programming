260c42f3a5b54ce078d09debf71be03e98e35fa9
Mabena490 <Mabenanaphty@gmail.com>
2023-08-02 05:04:25 -0700

    All Tasks

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
@@ -22,3 +21,4 @@ if __name__ == "__main__":
         session.delete(state)
     session.commit()
     session.close()
+    
